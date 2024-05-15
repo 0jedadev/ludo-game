81c0efc38b292a3d204114d6df183efb36bf89ef
Jônatas Ojeda <122798411+0jedadev@users.noreply.github.com>
2024-05-15 09:56:36 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,41 @@
 
 
-import ConfigsEngine from "./Configs/configs";
-import GraphicEngine from "./GraphicEngine/GraphicEngine";
-import PhysicsEngine from "./PhysicsEngine/PhysicsEngine";
-import GameLogicEngine from './GameLogic/GameLogic';
+import ConfigsEngine from "./LudoGame/Configs/configs";
+import GraphicEngine from "./LudoGame/GraphicEngine/GraphicEngine";
+import PhysicsEngine from "./LudoGame/PhysicsEngine/PhysicsEngine";
+import GameLogicEngine from './LudoGame/GameLogic/GameLogic';
+import GraphicEngineTicTacToe from "./TicTacToeGame/GraphicEngine/GraphicEngineTicTacToe";
+import GameEngineTicTacToe from "./TicTacToeGame/GameLogic/GameEngineTicTacToe";
+import FontsEngine from "./Fonts/FontsEngine";
 
 
 const configsEngine = new ConfigsEngine();
+configsEngine.x = 7;
+configsEngine.y = 4;
+configsEngine.z = 10;
 const graphicEngine = new GraphicEngine(configsEngine);
+const fontsEngine = new FontsEngine(graphicEngine);
 const physicsEngine = new PhysicsEngine(configsEngine);
 const gameLogicEngine = new GameLogicEngine(graphicEngine, physicsEngine, configsEngine);
+const graphicEngineTicTacToe = new GraphicEngineTicTacToe(graphicEngine, configsEngine);
+const gameEngineTicTacToe = new GameEngineTicTacToe(fontsEngine, graphicEngine, gameLogicEngine, graphicEngineTicTacToe, configsEngine);
 
 
-//ThreeJS
-graphicEngine.Floor();
-graphicEngine.callAddBorders();
-graphicEngine.planeBelowFloor();
-graphicEngine.callCircleBase();
-graphicEngine.callRoutes();
-graphicEngine.callDirectionalGames();
-graphicEngine.createGroundMesh();
-graphicEngine.createDice();
-graphicEngine.CallPinos();
-graphicEngine.lightsFunc();
-
-//Cannon
-physicsEngine.createGroundCannon();
-physicsEngine.createDiceCannon();
-physicsEngine.contactDice();
+//ThreeJS LudoGame
+graphicEngine.InitLudo();
 
 
-//Logic Game
-gameLogicEngine.InitGameButton();
-gameLogicEngine.callCreateWalls();
-gameLogicEngine.tick();
+//Cannon LudoGame
+physicsEngine.InitPhysics();
+
+
+//Logic LudoGame
+gameLogicEngine.InitGameLogic();
+
+
+//THREEJS Jogo da Velha
+graphicEngineTicTacToe.init();
+
+//Logi Jodo da Velha
+gameEngineTicTacToe.start();
+

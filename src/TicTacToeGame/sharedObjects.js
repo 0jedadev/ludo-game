be23dfeddf11f6658f127a6bb7cd77560a04859d
Jônatas Ojeda <122798411+0jedadev@users.noreply.github.com>
2024-05-15 09:52:42 -0300

    Create sharedObjects.js

---
+++ src/TicTacToeGame/sharedObjects.js
@@ -0,0 +1,21 @@
+// sharedObjects.js
+const sharedObjects = {
+    circles: {},
+    cross: {},
+    fonts: {},
+    bases: {
+        'initialBaseX': { x: 11, y: 0, z: -2 },
+        'initialBase0': { x: 19, y: 0, z: -2 },
+        '1': { x: 13, y: 0, z: -2 },
+        '2': { x: 15, y: 0, z: -2 },
+        '3': { x: 17, y: 0, z: -2 },
+        '4': { x: 13, y: 0, z: 0 },
+        '5': { x: 15, y: 0, z: 0 },
+        '6': { x: 17, y: 0, z: 0 },
+        '7': { x: 13, y: 0, z: 2 },
+        '8': { x: 15, y: 0, z: 2 },
+        '9': { x: 17, y: 0, z: 2 },
+    },
+};
+
+export default sharedObjects; 
